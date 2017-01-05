036cad4ef559036df73a84efce5fa05b6b48ce1a
Wesley Tan <wesleyt1998@gmail.com>
2017-01-04 19:42:45 -0500

    ranked icons and beginning statistics

--- main.js
+++ main.js
@@ -51,6 +51,7 @@ function summonerLookup(playerNum) {
         "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/profileicon/"+ profileIconNum+ ".png");
         document.getElementById('summonerName' + playerNum).innerHTML = SUMMONER_NAME;
         getSummonerRank(summonerId, playerNum, REGION);
+        getPlayerStats(summonerId, REGION)
         if (playerNum == 1) {
           $('#player1MainInfo').show();
         } else if (playerNum == 2) {
@@ -76,18 +77,63 @@ function getSummonerRank(summonerId, playerNum, reg) {
       tier = tier.toLowerCase().split('')
       tier[0] = tier[0].toUpperCase();
       tier = tier.join('');
-
-      var division = json[summonerId][0].entries[0].division;
+      if (tier === "Master" || tier === "Challenger")
+        var division = "";
+      else
+        var division = json[summonerId][0].entries[0].division;
       var lp = json[summonerId][0].entries[0].leaguePoints;
       var wins = json[summonerId][0].entries[0].wins;
       var losses = json[summonerId][0].entries[0].losses
       var winRate = wins / (wins + losses) * 100;
+      winRate = winRate.toFixed(2);
+      var rankedIconLocation = setRankedIcon(tier, division);
+      console.log(rankedIconLocation);
+      $('.hr-'+playerNum).show();
       document.getElementById('summoner' + playerNum + 'Rank').innerHTML = tier + " " + division + " - " + lp + " LP";
       document.getElementById('summoner' + playerNum + 'WinRatio').innerHTML = "Win Ratio: " + winRate + "% " +
-       "(" + wins + "W " + losses + "L)" ;
+       "<br/>(" + wins + "W " + losses + "L)" ;
+       document.getElementById('summoner' + playerNum +'RankIcon').setAttribute("src", rankedIconLocation);
     },
     error: function (XMLHttpRequest, textStatus, errorThrown) {
-      alert("Error getting Summoner data !");
+      if (errorThrown === "Not Found") {
+        document.getElementById('summoner' + playerNum + 'Rank').innerHTML = "Unranked";
+        document.getElementById('summoner' + playerNum + 'WinRatio').innerHTML = "Win Ratio: n/a ";
+        document.getElementById('summoner' + playerNum +'RankIcon').setAttribute("src", "./base_icons/provisional.png");
+      }
+    }
+  });
+}
+
+function setRankedIcon(tier, division) {
+  tier = tier.toLowerCase();
+  division = division.toLowerCase();
+  if (tier === "master" || tier === "challenger")
+    var location = "./base_icons/" + tier + ".png";
+  else
+    var location = "./tier_icons/" + tier + "_" + division + ".png";
+  return location
+}
+
+function getPlayerStats(summonerId, reg) {
+  $.ajax({
+    url: "https://na.api.pvp.net/api/lol/"+ reg + "/v1.3/stats/by-summoner/"+ summonerId + "/ranked?api_key=" + API_KEY,
+//    https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/20502760/ranked?season=SEASON2017&api_key=RGAPI-217a0491-95a0-4107-9d45-93ac21a49b94
+    type: 'GET',
+    data: {
+
+    },
+    success: function(json) {
+      var data = json.champions;
+      var totals = data[data.length - 1];
+      var totalKills;
+      var totalDeaths;
+      var totalAssists;
+      var totalCS;
+      var totalGames;
+      console.log(last.id);
+    },
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+
     }
   });
 }
