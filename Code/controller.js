9da70fa6f24cda5782aef95d1fb6fe5d69cddf6c
Wes T4N <wesleyt1998@gmail.com>
2017-09-14 13:08:05 -0400

    added gitignore

--- Code/controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('DemoApp').controller('ParentCtrl', function() {
-  var Prt = this;
-  Prt.name = 'John';
-}]).controller('SonCtrl', function() {
-  var Son = this;
-  Son.name = 'Sam';
-}])
